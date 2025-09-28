34b0c87aa9ceefa3c8efe71ec833d0f4bfa77a06
wilsonadrianpinzon-a11y <wilsonadrianpinzon@gmail.com>
2025-09-28 12:42:30 -0500

    app2.py

--- app2.py
+++ app2.py
@@ -33,4 +33,5 @@ def whatsapp_reply():
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
+
